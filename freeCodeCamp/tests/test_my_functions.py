b8c60f97dff6ed66d9f241cfdf7d00e7ed8ca0fc
Anas Kaarov <anasbinkurban@gmail.com>
2024-08-04 17:45:32 +0600

    feat: first tests

---
+++ freeCodeCamp/tests/test_my_functions.py
@@ -0,0 +1,22 @@
+import pytest
+from source import my_functions
+
+
+def test_add():
+    result = my_functions.add(1, 4)
+    assert result == 5
+
+
+def test_add_strings():
+    result = my_functions.add("1 like ", "burgers")
+    assert result == "1 like burgers"
+
+
+def test_divide():
+    result = my_functions.divide(10, 5)
+    assert result == 2
+
+
+def test_divide_by_zero():
+    with pytest.raises(ZeroDivisionError):
+        my_functions.divide(10, 0)
